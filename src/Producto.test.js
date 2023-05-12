a405908820c4985cf4a2020424e734be063de144
Adriel Mounzón <adriel.mounzon@ucb.edu.bo>
2023-05-12 09:28:08 -0400

    Crear una lista de productos y agregar varios productos

--- src/Producto.test.js
+++ src/Producto.test.js
@@ -18,4 +18,15 @@ describe("Producto", () => {
     listaProd.añadirProducto(prod1);
     expect(listaProd.Lista.length).toEqual(1);
   });
+
+    it("deberia agregar varios productos a la lista", () => {
+      let prod1= new Producto(1, "Galletas", "Galletas de chocolate con relleno de crema");
+      let prod2= new Producto(2, "Cereales", "Cereales Fruit Loops");
+      let prod3= new Producto(3, "Coca Cola", "Coca Cola de 300 ml.");
+      let listaProd= new ListaProductos();
+      listaProd.añadirProducto(prod1);
+      listaProd.añadirProducto(prod2);
+      listaProd.añadirProducto(prod3);
+      expect(listaProd.Lista.length).toEqual(3);
+  });
 });
