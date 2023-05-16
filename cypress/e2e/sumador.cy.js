b69daaf919ec514278f0e6c12499873331b2cb21
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-05-16 15:25:11 -0400

    Se refactorizo una prueba de cypress para que pase la prueba mostrando el Stock de un producto mas

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -16,7 +16,7 @@ describe("index", () => {
     cy.get("#descripcion").type("Cereales Fruit Loops");
     cy.get("#guardar-button").click();
     cy.get("#resultadoProducto-div").should("contain", "Id: 2, Nombre: Cereales, Descripción: Cereales Fruit Loops");
-    cy.get("#resultadoLista-div").should("contain", "Id: 1, Nombre: Galletas, Descripción: Galletas de chocolate con relleno de crema, Precio: 0; Id: 2, Nombre: Cereales, Descripción: Cereales Fruit Loops, Precio: 0")
+    cy.get("#resultadoLista-div").should("contain", "Id: 1, Nombre: Galletas, Descripción: Galletas de chocolate con relleno de crema, Precio: 0, Stock: 0; Id: 2, Nombre: Cereales, Descripción: Cereales Fruit Loops, Precio: 0, Stock: 0")
   }); 
 
   it("Verifica que el menu este vacio mientras no se hayan creado productos", () => {
