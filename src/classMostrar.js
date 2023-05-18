329ef04e9a4254a2531eee3e98327b93c77c9f56
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-05-18 08:45:50 -0400

    Se refactorizo la funcionalidad de reservar un producto para que al momento de reservar o cancelar se muestre el stock actualizado del producto en el menu

--- src/classMostrar.js
+++ src/classMostrar.js
@@ -10,29 +10,31 @@ class Mostrar{
         }
         const ListaAMostrar=listaProd.Lista;
         ListaAMostrar.forEach((producto, index) => {
-            
+
             if(producto.tieneStock())
             {
-            const li=document.createElement("li");
-            const botonReservar = document.createElement("button");
-            botonReservar.innerText="Reservar";
-            const content = document.createElement("p");
-            content.innerHTML="<b> Nombre del Producto: </b> "+ producto.Nombre +"<br> <b>  Descripcion: </b> "+producto.Descripcion;
-            if(producto.tienePrecio()) content.innerHTML+="<br> <b>  Precio: </b> "+producto.Precio;
-            if(producto.tieneStock()) content.innerHTML+="<br> <b>  Stock: </b> "+producto.Stock;
-            content.appendChild(botonReservar);
-            li.appendChild(content);
-            divMenu.appendChild(li);
+                const li=document.createElement("li");
+                const botonReservar = document.createElement("button");
+                botonReservar.innerText="Reservar";
+                const content = document.createElement("p");
+                content.innerHTML="<b> Nombre del Producto: </b> "+ producto.Nombre +"<br> <b>  Descripcion: </b> "+producto.Descripcion;
+                if(producto.tienePrecio()) content.innerHTML+="<br> <b>  Precio: </b> "+producto.Precio;
+                if(producto.tieneStock()) content.innerHTML+="<br> <b>  Stock: </b> "+producto.Stock;
+                content.appendChild(botonReservar);
+                li.appendChild(content);
+                divMenu.appendChild(li);
 
-            botonReservar.onclick=()=>{
-                let producto=listaProd.Lista[index];
-                listaProd.reserva.añadirProducto(producto);
-                producto.decrementarStock(1);
-                alert("Se reservó el producto escogido");
-            }}
+                botonReservar.onclick=()=>{
+                    let producto=listaProd.Lista[index];
+                    listaProd.reserva.añadirProducto(producto);
+                    producto.decrementarStock(1);
+                    alert("Se reservó el producto escogido");
+                    this.ListaProductos(listaProd);
+                }
+            }
         });
     }
-    Reservas(reserva)
+    Reservas(reserva,listaProd)
     {
         divReserva.innerText="";
         const content = document.createElement("p");
@@ -45,6 +47,7 @@ class Mostrar{
             reserva.CancelarReserva();
             alert("Se canceló la reserva");
             this.Reservas(reserva);
+            this.ListaProductos(listaProd);
         }
     }
 }
--- src/presenter.js
+++ src/presenter.js
@@ -8,16 +8,16 @@ const stock = document.querySelector("#stock");
 const form = document.querySelector("#crearProducto-form");
 const divProducto = document.querySelector("#resultadoProducto-div");
 const divLista = document.querySelector("#resultadoLista-div");
-const listaMenu = document.getElementById("menuDisponible");
 const menuRef = document.getElementById("menu-ref");
 const prodRef = document.getElementById("productos-ref");
 const formVerReserva=document.querySelector("#MostrarReserva-form");
 const divReserva=document.getElementById("divReserva");
-const divMenu=document.getElementById("divMenu");
+
 let listaProd = new ListaProductos();
 let contadorId = 1;
 let productoAAñadir;
 let mostrar=new Mostrar();
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -35,6 +35,7 @@ form.addEventListener("submit", (event) => {
     descripcion.value = "";
     precio.value = 0;
     stock.value = 0;
+    mostrar.ListaProductos(listaProd);
   }
   else
   {
@@ -44,15 +45,16 @@ form.addEventListener("submit", (event) => {
 });
 
 menuRef.addEventListener('click',function(){
-  
   mostrar.ListaProductos(listaProd);
 })
+
 prodRef.addEventListener('click',function(){
   divProducto.innerHTML = productoAAñadir.aTextoConStock();
   divLista.innerHTML = listaProd.aTexto();
 })
+
 formVerReserva.addEventListener("submit", (event) => {
   event.preventDefault();
   divReserva.innerText="";
-  mostrar.Reservas(listaProd.reserva)
+  mostrar.Reservas(listaProd.reserva,listaProd)
 });
