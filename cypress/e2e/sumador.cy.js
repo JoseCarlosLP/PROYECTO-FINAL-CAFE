56e553dca87ae215cfe269a36c78a32cf1fb6b6b
Isabel <isabqm121@gmail.com>
2023-05-31 22:36:17 -0400

    Prueba UI: verificar que al cambiar la vista, si se muestra la de administrador

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -8,4 +8,11 @@ describe("index", () => {
     cy.visit("http://localhost:1234/#seccion-menu");
     cy.get("#divMenu").should('be.empty')
   });
+  it("Verifica que cambie a la vista Administrador", () => {
+    cy.visit("/");
+    cy.get("#cambiarVista").click();
+    cy.get("#vista_admin").should('not.have.css','display','block');
+    cy.get("#vista_menu").should('not.have.css','display','none');
+  });
+
 });
