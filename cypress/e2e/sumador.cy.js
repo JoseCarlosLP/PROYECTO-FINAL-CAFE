d5f6db4e44a581c2f14ff0690d3d3446c70a188d
Isabel <isabqm121@gmail.com>
2023-05-31 22:19:40 -0400

    Prueba UI: verificar que al cargar la pagina, se muestre el menu de productos para la vista de usuario

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -1,23 +1,8 @@
 describe("index", () => {
-  xit("Verifica que los valores ingresados de un producto se lean correctamente", () => {
+  it("Verifica que al cargar la pagina, se muestre la pantalla de menu para cliente", () => {
     cy.visit("/");
-    cy.get("#nombre").type("Galletas");
-    cy.get("#descripcion").type("Galletas de chocolate con relleno de crema");
-    cy.get("#guardar-button").click();
-    cy.get("#resultadoProducto-div").should("contain", "Id: 1, Nombre: Galletas, Descripción: Galletas de chocolate con relleno de crema");
+    cy.get("#vista_menu").should("contain", "MENU");
   });
-
-  xit("Verifica que los valores ingresados de varios productos se lean correctamente", () => {
-    cy.visit("/");
-    cy.get("#nombre").type("Galletas");
-    cy.get("#descripcion").type("Galletas de chocolate con relleno de crema");
-    cy.get("#guardar-button").click();
-    cy.get("#nombre").type("Cereales");
-    cy.get("#descripcion").type("Cereales Fruit Loops");
-    cy.get("#guardar-button").click();
-    cy.get("#resultadoProducto-div").should("contain", "Id: 2, Nombre: Cereales, Descripción: Cereales Fruit Loops");
-    cy.get("#resultadoLista-div").should("contain", "Id: 1, Nombre: Galletas, Descripción: Galletas de chocolate con relleno de crema, Precio: 0, Stock: 0; Id: 2, Nombre: Cereales, Descripción: Cereales Fruit Loops, Precio: 0, Stock: 0")
-  }); 
 
   it("Verifica que el menu este vacio mientras no se hayan creado productos", () => {
     cy.visit("http://localhost:1234/#seccion-menu");
